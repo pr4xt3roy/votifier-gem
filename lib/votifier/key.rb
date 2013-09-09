8797e1aca268061c9173fd6b5a50ebe444add57a
Pr4xt3roy <pr4xt3roy@capricatown.net>
2013-09-09 12:48:28 +0000

    Remove the redundent method from_public_key_file and added a new method import that receive all kinds of format.

--- lib/votifier/key.rb
+++ lib/votifier/key.rb
@@ -7,13 +7,28 @@ module Votifier
       @key = OpenSSL::PKey::RSA.new(pem_key_content)
     end
 
-    def self.from_pem_key_file(key_file)
-      key_content = File.read(key_file)
-      self.from_perm_key_content(key_content)
-    end
-
-    def self.from_public_key_file(key_file)
-      self.from_key_file(key_file, :public)
+    # Import a RSA key from a filename, a File objetc or a string containing the key.
+    #
+    # Example Usage:
+    #  key = Voritifer::Key.import(file_name)
+    #
+    # @param  [Mixed] key An RSA private or public key as a filename, a File object or a String.
+    #   The format can be PEM (with the -- BEGIN headers) or plaim
+    # @returns [Voritifer::Key] The object representing the key
+    def self.import(key)
+      if key.respond_to?(:read)
+        self.from_pem_key_content(key.read)
+      elsif key.match('pub.*\.key$')
+        self.from_key_file(key, :public)
+      elsif key.match('priv.*\.key$')
+        self.from_key_file(key, :private)
+      elsif key.match('.*\.pem$')
+        self.from_pem_key_file(key)
+      elsif key.respond_to?(:to_s)
+        self.from_pem_key_content(key.to_s)
+      else
+        raise 'Invalid key file format. Support formats: public.key, private.key, key.pem or string.'
+      end
     end
 
     def self.from_key_file(key_file, type = :private)
@@ -22,8 +37,13 @@ module Votifier
     end
 
     def self.from_key_content(key_content, type = :private)
-      pem_key_content = convert_to_pem_format(key_content, type)
-      from_pem_key_content(pem_key_content)
+      pem_key_content = self.convert_to_pem_format(key_content, type)
+      self.from_pem_key_content(pem_key_content)
+    end
+
+    def self.from_pem_key_file(key_file)
+      key_content = File.read(key_file)
+      self.from_pem_key_content(key_content)
     end
 
     def self.from_pem_key_content(pem_key_content)
