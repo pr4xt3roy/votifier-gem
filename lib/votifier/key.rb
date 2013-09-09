482de615e3c53bacc58ddb21585f99a78b864113
pr4xt3roy <pr4xt3roy@capricatown.net>
2013-09-09 09:55:45 -0400

    Add documentation to Key, Modified Key#import and added tests. The tests are for the Key class. The import was modified to support a lot more possible formats and different input types.

--- lib/votifier/key.rb
+++ lib/votifier/key.rb
@@ -3,6 +3,8 @@ require 'openssl'
 module Votifier
   class Key
 
+    # Create a new key from a PEM formated string
+    # @param [String] pem_key_content The PEM formated string
     def initialize(pem_key_content)
       @key = OpenSSL::PKey::RSA.new(pem_key_content)
     end
@@ -17,7 +19,7 @@ module Votifier
     # @returns [Voritifer::Key] The object representing the key
     def self.import(key)
       if key.respond_to?(:read)
-        self.from_pem_key_content(key.read)
+        self.from_unknown_key_content(key.read)
       elsif key.match('pub.*\.key$')
         self.from_key_file(key, :public)
       elsif key.match('priv.*\.key$')
@@ -25,33 +27,72 @@ module Votifier
       elsif key.match('.*\.pem$')
         self.from_pem_key_file(key)
       elsif key.respond_to?(:to_s)
-        self.from_pem_key_content(key.to_s)
+        self.from_unknown_key_content(key.to_s)
       else
         raise 'Invalid key file format. Support formats: public.key, private.key, key.pem or string.'
       end
     end
 
+    # Import a key from a string or array and detect what format and type the key is.
+    # @param [Array,String] key_content The key content as a string (or array of lines)
+    # @return [Votifier::Key] The key that was imported.
+    def self.from_unknown_key_content(key_content)
+      if key_content.is_a?(Array)
+        if key_content[0].match(/\n/)
+          key_content = key_content.join("")
+        else
+          key_content = key_content.join("\n")
+        end
+      end
+      if key_content.match(/^\-\-\-\-\-BEGIN /)
+        self.from_pem_key_content(key_content)
+      else
+        if key_content.bytesize > 1000
+          self.from_key_content(key_content, :private)
+        else
+          self.from_key_content(key_content, :public)
+        end
+      end
+    end
+
+    # Import a non-PEM key from a filename
+    # @param [String] key_file The filename of the key without the PEM headers.
+    # @param [Symbol] type The type of key, Either :public or :private. Defaults to :private
+    # @return [Votifier::Key] The imported key
     def self.from_key_file(key_file, type = :private)
       key_content = File.read(key_file)
       self.from_key_content(key_content, type)
     end
 
+    # Import a non-PEM key from a String
+    # @param [String] key_content The actual key content in a non-PEM format.
+    # @param [Symbol] type The type of key, Either :public or :private. Defaults to :private
+    # @return [Votifier::Key] The imported key
     def self.from_key_content(key_content, type = :private)
       pem_key_content = self.convert_to_pem_format(key_content, type)
       self.from_pem_key_content(pem_key_content)
     end
 
+    # Import a PEM key from a filename
+    # @param [String] key_file The filename of the PEM key
+    # @return [Votifier::Key] The imported key
     def self.from_pem_key_file(key_file)
       key_content = File.read(key_file)
       self.from_pem_key_content(key_content)
     end
 
+    # Import a PEM key from a string
+    # @param [String] key_content The actual key content in a PEM format.
+    # @return [Votifier::Key] The imported key
     def self.from_pem_key_content(pem_key_content)
-      key = self.new(pem_key_content)
-      key
+      self.new(pem_key_content)
     end
 
-    def self.convert_to_pem_format(key_content, type = :private)
+    # Convert a non-PEM key string into a PEM key format string
+    # @param [String] key_content The string of the non-PEM key
+    # @param [Symbol] type The type of key, Either :public or :private.
+    # @return [String] The key in a PEM format
+    def self.convert_to_pem_format(key_content, type)
       key_headers = {
         :private => "RSA PRIVATE",
         :public => "PUBLIC"
--- test/test_votifer_key.rb
+++ test/test_votifer_key.rb
@@ -41,4 +41,82 @@ class VotifierKeyTest < Test::Unit::TestCase
     assert !key.nil?
   end
 
+  def test_import_public_pem_string
+    public_pem_file_name = File.expand_path('../../config/sample-public.pem', __FILE__)
+    public_pem_file_string = File.open(public_pem_file_name, 'r').read
+    key = nil
+    assert_nothing_raised do
+      key = Votifier::Key.import(public_pem_file_string)
+    end
+    assert !key.nil?
+  end
+
+  def test_import_private_pem_string
+    private_pem_file_name = File.expand_path('../../config/sample-private.pem', __FILE__)
+    private_pem_file_string = File.open(private_pem_file_name, 'r')
+    key = nil
+    assert_nothing_raised do
+      key = Votifier::Key.import(private_pem_file_string)
+    end
+    assert !key.nil?
+  end
+
+  def test_import_public_non_pem_filename
+    public_non_pem_file_name = File.expand_path('../../config/sample-public.key', __FILE__)
+    key = nil
+    assert_nothing_raised do
+      key = Votifier::Key.import(public_non_pem_file_name)
+    end
+    assert !key.nil?
+  end
+
+  def test_import_private_non_pem_filename
+    private_non_pem_file_name = File.expand_path('../../config/sample-private.key', __FILE__)
+    key = nil
+    assert_nothing_raised do
+      key = Votifier::Key.import(private_non_pem_file_name)
+    end
+    assert !key.nil?
+  end
+
+  def test_import_public_non_pem_file_object
+    public_non_pem_file_name = File.expand_path('../../config/sample-public.key', __FILE__)
+    public_non_pem_file_obj = File.open(public_non_pem_file_name, 'r')
+    key = nil
+    assert_nothing_raised do
+      key = Votifier::Key.import(public_non_pem_file_obj)
+    end
+    assert !key.nil?
+  end
+
+  def test_import_private_non_pem_file_object
+    private_non_pem_file_name = File.expand_path('../../config/sample-private.key', __FILE__)
+    private_non_pem_file_obj = File.open(private_non_pem_file_name, 'r')
+    key = nil
+    assert_nothing_raised do
+      key = Votifier::Key.import(private_non_pem_file_obj)
+    end
+    assert !key.nil?
+  end
+
+  def test_import_public_non_pem_string
+    public_non_pem_file_name = File.expand_path('../../config/sample-public.key', __FILE__)
+    public_non_pem_file_string = File.open(public_non_pem_file_name, 'r').read
+    key = nil
+    assert_nothing_raised do
+      key = Votifier::Key.import(public_non_pem_file_string)
+    end
+    assert !key.nil?
+  end
+
+  def test_import_private_non_pem_string
+    private_non_pem_file_name = File.expand_path('../../config/sample-private.key', __FILE__)
+    private_non_pem_file_string = File.open(private_non_pem_file_name, 'r')
+    key = nil
+    assert_nothing_raised do
+      key = Votifier::Key.import(private_non_pem_file_string)
+    end
+    assert !key.nil?
+  end
+
 end
